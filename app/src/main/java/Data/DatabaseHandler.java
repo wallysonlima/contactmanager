f56cc07b6dfd292dc68713c14db67a2171856f25
root <root@localhost.localdomain>
2018-12-08 15:41:22 -0200

    first commit

---
+++ app/src/main/java/Data/DatabaseHandler.java
@@ -0,0 +1,22 @@
+package Data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHandler extends SQLiteOpenHelper {
+
+    public DatabaseHandler(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
---
+++ app/src/main/java/Utils/Util.java
@@ -0,0 +1,14 @@
+package Utils;
+
+public class Util {
+
+    // Database Version
+    private static final int DATEBASE_VERSION = 1;
+    private static final String DATABASE_NAME = "contactsDB";
+    private static final String TABLE_NAME = "contacts";
+
+    // Contacts table columns names
+    private static final String KEY_ID = "id";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_PHONE_NUMBER = "phone_number";
+}
